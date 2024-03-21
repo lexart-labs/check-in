25188420fcc1e801b5ebfe05fc2a3b57a0b461be
JoaoRViana <joaorviana98@gmail.com>
2024-03-21 16:24:08 -0300

    fix: changed item.dev_email for item.devEmail

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -1,14 +1,12 @@
 <template>
     <v-layout class="rounded rounded-md">
         <v-app-bar title="Check-in/Brb" theme="dark"></v-app-bar>
-
         <v-navigation-drawer permanent theme="dark">
             <v-list>
                 <v-list-item title="CheckIn/Brb" @click="goTo('/check-in')"></v-list-item>
                 <v-list-item title="Logout" @click="logOut"></v-list-item>
             </v-list>
         </v-navigation-drawer>
-
         <v-main class="d-flex align-center justify-center" style="min-height: 100vh;">
             <v-card 
                 class="mx-auto check-in--card"
@@ -32,13 +30,11 @@
                     variant="solo-filled"
                 ></v-text-field>
             </v-card-title>
-        
             <v-divider></v-divider>
             <v-data-table v-model:search="search" :items="items">
                 <template v-slot:header.isCheckIn>
                     <div class="text-end">Status</div>
                 </template>
-        
                 <template v-slot:item.isCheckIn="{ item }">
                     <div class="text-end">
                         <v-btn
@@ -145,7 +141,7 @@ async function sendReason(){
   let docId = null;
   docSnap.docs.forEach((i)=>{
     let item = i.data()
-    if(item.dev_email===user.email){
+    if(item.devEmail===user.email){
       exist = true
       docId = i.id
     }
